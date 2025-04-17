e2e10f05c0f32ce15bf7fcd098edc0c1be00227f
jxch <qianbuhan@proton.me>
2025-04-18 00:10:29 +0800

    优化 Docker 构建脚本，移除不必要的标签和推送命令

--- docker/docker-build-proxychains.ps1
+++ docker/docker-build-proxychains.ps1
@@ -1,1 +1,1 @@
-proxychains docker buildx build --platform=linux/arm64,linux/amd64 -t jxch/zhiban-clock:$(Get-Date -Format 'yyyyMMddHH') -t jxch/zhiban-clock:latest . --push
+proxychains docker buildx build --platform=linux/arm64,linux/amd64 -t jxch/zhiban-clock:$(Get-Date -Format 'yyyyMMddHH') . --push
--- native/native-build.ps1
+++ native/native-build.ps1
@@ -1,8 +1,6 @@
 mvn clean -Pnative -Prelease spring-boot:build-image -f pom.xml
 
-docker tag zhiban.clock:1.0.0 jxch/zhiban-clock:$(Get-Date -Format 'yyyyMMddHH')
 docker tag zhiban.clock:1.0.0 jxch/zhiban-clock:1.0.0
 docker tag zhiban.clock:1.0.0 jxch/zhiban-clock:latest
-docker push jxch/zhiban-clock:$(Get-Date -Format 'yyyyMMddHH')
 docker push jxch/zhiban-clock:1.0.0
 docker push jxch/zhiban-clock:latest
